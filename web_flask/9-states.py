f83af1baa9bec87049be96d6967422bf6f0c8fb3
R-Owino <sayeakinyi@gmail.com>
2023-04-23 08:23:34 -0700

    Display state and state id if available

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -9,32 +9,32 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/states", strict_slashes=False)
+@app.route('/states', strict_slashes=False)
 def states():
     """
-    Displays a HTML page with a list of all State objects sorted by name
+    Display a HTML page with a list of States
     """
-    states = storage.all("State")
-    return render_template("9-states.html", states=states)
+    states = storage.all('State')
+    return render_template('9-states.html', states=states)
 
 
-@app.route("/states/<id>", strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
 def states_id(id):
     """
-    Displays a HTML page with info about <id>
-    i.e states and its cities if available
+    Display a HTML page with the State and it's Cities
     """
-    for state in storage.all("State").values():
+    states = storage.all('State')
+    for state in states.values():
         if state.id == id:
-            return render_template("9-states.html", state=state, id=id)
-    return render_template("9-states.html")
+            return render_template('9-states.html', state=state, id=id)
+    return render_template('9-states.html')
 
 
 @app.teardown_appcontext
-def teardown(exception):
-    """Remove the current SQLAlchemy session."""
+def teardown_db(exception):
+    """Remove the current SQLAlchemy session"""
     storage.close()
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host='0.0.0.0', port=5000)
