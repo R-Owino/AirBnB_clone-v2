189255de552099f35fa704ca77936e39160251ba
R-Owino <sayeakinyi@gmail.com>
2023-04-23 05:03:42 -0700

    Update: DB storage

---
+++ models/engine/db_storage.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+"""Defines the new database storage for our models"""
+from os import getenv
+from models.amenity import Amenity
+from models.base_model import BaseModel, Base
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session, relationship
+
+
+class DBStorage:
+    """Defines our hbnb_dev_db"""
+    # Private class attributes
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialize the new database"""
+        HBNB_MYSQL_USER = getenv('HBNB_MYSQL_USER')
+        HBNB_MYSQL_PWD = getenv('HBNB_MYSQL_PWD')
+        HBNB_MYSQL_HOST = getenv('HBNB_MYSQL_HOST')
+        HBNB_MYSQL_DB = getenv('HBNB_MYSQL_DB')
+        HBNB_ENV = getenv('HBNB_ENV')
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            HBNB_MYSQL_USER, HBNB_MYSQL_PWD, HBNB_MYSQL_HOST, HBNB_MYSQL_DB),
+                                      pool_pre_ping=True)
+        if HBNB_ENV == "test":
+            Base.metadata.drop_all(self.engine)
+
+    def all(self, cls=None):
+        """Query the Database Session"""
+        if cls is None:
+            objs = self.__session.query(State).all()
+            objs.extend(self.__session.query(City).all())
+            objs.extend(self.__session.query(User).all())
+            objs.extend(self.__session.query(Place).all())
+            objs.extend(self.__session.query(Review).all())
+            objs.extend(self.__session.query(Amenity).all())
+
+        else:
+            if type(cls) == str:
+                cls = eval(cls)
+            objs = self.__session.query(cls)
+        return {"{}.{}".format(type(o).__name__, o.id): o for o in objs}
+
+    def new(self, obj):
+        """Add a new object to the database"""
+        self.__session.add(obj)
+
+    def save(self):
+        """Saves the changes made to the database"""
+        self.__session.commit()
+
+    def delete(self, obj=None):
+        """delete obj if exists"""
+        if obj is not None:
+            self.session.delete(obj)
+
+    def reload(self):
+        """Creates all the tables from the database"""
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        Session = scoped_session(session_factory)
+        self.__session = Session()
+
+    def classes(self):
+        """Returns a dictionary of valid classes and their references."""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        classes = {"BaseModel": BaseModel,
+                   "User": User,
+                   "State": State,
+                   "City": City,
+                   "Amenity": Amenity,
+                   "Place": Place,
+                   "Review": Review}
+        return classes
+
+    def close(self):
+        """Closes the current session"""
+        self.__session.close()
